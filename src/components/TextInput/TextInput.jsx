a52f9ca58e0f24bd373516901d0074544c6b8dd7
davidhorvat <david.horvat@gauss.dev>
2024-04-13 09:03:54 +0200

    refactor: text input component

--- src/components/TextInput/TextInput.jsx
+++ src/components/TextInput/TextInput.jsx
@@ -2,6 +2,7 @@ import "./TextInput.css"
 import iconVisible from "../../assets/visible.png"
 import iconHidden from "../../assets/hidden.png"
 import {useState} from "react";
+import PropTypes from "prop-types";
 function TextInput({onChange, value, label, placeholder, type, icon, id}){
 
     const [inputType, setInputType] = useState(type)
@@ -15,9 +16,19 @@ function TextInput({onChange, value, label, placeholder, type, icon, id}){
         <div className={"text-input"}>
             {label && <label>{label}</label>}
             <input type={inputType} placeholder={placeholder} id={id} onChange={e => onChange(e)} value={value}/>
-            {icon && <img id={"icon"} src={inputIcon} onClick={togleInputType}/>}
+            {icon && <img id={"icon"} src={inputIcon} onClick={togleInputType} alt={""}/>}
         </div>
     )
 }
+
+TextInput.propTypes = {
+    value: PropTypes.string,
+    icon: PropTypes.string,
+    label: PropTypes.string,
+    placeholder: PropTypes.string,
+    type: PropTypes.string,
+    id: PropTypes.string,
+    onChange: PropTypes.func,
+};
 
 export default TextInput
