32af064aae7e61f2b844d5a107729ec5bff623a3
davidhorvat <david.horvat@gauss.dev>
2024-05-21 20:13:03 +0200

    chore: add home page

---
+++ src/components/Entity/Entity.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import './Entity.css';
+import PropTypes from "prop-types";
+
+function Entity({ name, emailAddress, password, username, description, iconPath}) {
+    const [isExpanded, setIsExpanded] = useState(false);
+
+    const handleCopyText = () => {
+        navigator.clipboard.writeText(password).then(() => {
+            alert('Text copied to clipboard!');
+        }).catch(err => {
+            console.error('Failed to copy text: ', err);
+        });
+    };
+
+    const handleToggleExpand = () => {
+        setIsExpanded(!isExpanded);
+    };
+
+    return (
+        <div className={`entity-component ${isExpanded ? 'expanded' : ''}`}>
+            <h2>{name}</h2>
+            <img src={iconPath} alt={name} className="entity-picture" />
+            <div className="text-container">
+                <p>{password}</p>
+                <button onClick={handleCopyText} className="copy-icon">📋</button>
+            </div>
+            <p>{description}</p>
+            <button onClick={handleToggleExpand} className="expand-icon">
+                {isExpanded ? '🔼' : '🔽'}
+            </button>
+            {isExpanded && (
+                <div className="additional-info">
+                    {username && <p>Username : {username}</p>}
+                    {emailAddress && <p>Email address : {emailAddress}</p>}
+                </div>
+            )}
+        </div>
+    );
+}
+
+Entity.propTypes = {
+    name: PropTypes.string.isRequired,
+    password: PropTypes.string.isRequired,
+    email_address: PropTypes.string,
+    username: PropTypes.string,
+    description: PropTypes.string.isRequired,
+    iconPath: PropTypes.string,
+};
+export default Entity;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,43 @@
+import {useEffect, useState} from "react";
+import {useNavigate} from "react-router-dom";
+import Axios from "axios";
+import Entity from "../../components/Entity/Entity.jsx";
 
 function Home(){
+
+
+    const [entities, setEntities] = useState([]);
+
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        Axios.get("http://localhost:8085/entity/list", {withCredentials: true})
+            .then((response) => {
+                const jsonObject = JSON.parse(response.data);
+                setEntities(jsonObject.entities);
+            })
+            .catch((error) => {
+                console.log("evo ga", JSON.stringify(error));
+            });
+    }, []);
+
     return(
         <>
-            <h1>Home</h1>
+            {entities && (
+                <div>
+                    {entities.map((entity, index) => (
+                        <Entity
+                            key={entity.uuid}
+                            name={entity.name}
+                            iconPath={entity.icon_path}
+                            password={entity.password}
+                            emailAddress={entity.email_address}
+                            username={entity.username}
+                            description={entity.description}
+                        />
+                    ))}
+                </div>
+            )}
         </>
 
     )
