495e166ed06665a3bab6389396463cba5aac5660
davidhorvat <david.horvat@gauss.dev>
2024-05-29 16:47:54 +0200

    chore: decryption

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,9 +2,33 @@ import {useEffect, useState} from "react";
 import {useNavigate} from "react-router-dom";
 import Axios from "axios";
 import Entity from "../../components/Entity/Entity.jsx";
+import CryptoJS from 'crypto-js';
 
 function Home(){
 
+    function decodeBase64(input) {
+        return CryptoJS.enc.Base64.parse(input);
+    }
+
+    const decryptResponse = (cipherText) => {
+
+        const secret = 'abc&1*~#^2^#s0^=)^^7%b34';
+        const iv = '232e39185523184a5723586242200e05';
+
+
+        const key = CryptoJS.enc.Utf8.parse(secret);
+        const iv1 = CryptoJS.enc.Hex.parse(iv);
+
+        const decrypted = CryptoJS.AES.decrypt({
+            ciphertext: decodeBase64(cipherText.data)
+        }, key, {
+            iv: iv1,
+            mode: CryptoJS.mode.CFB,
+            padding: CryptoJS.pad.NoPadding
+        });
+
+        return CryptoJS.enc.Utf8.stringify(decrypted);
+    };
 
     const [entities, setEntities] = useState([]);
 
@@ -13,11 +37,11 @@ function Home(){
     useEffect(() => {
         Axios.get("http://localhost:8085/entity/list", {withCredentials: true})
             .then((response) => {
-                const jsonObject = JSON.parse(response.data);
-                setEntities(jsonObject.entities);
+                const decryptedResponse = decryptResponse(response);
+                console.log('Decrypted Response:', decryptedResponse);
             })
             .catch((error) => {
-                console.log("evo ga", JSON.stringify(error));
+                console.log(error);
             });
     }, []);
 
@@ -25,7 +49,7 @@ function Home(){
         <>
             {entities && (
                 <div>
-                    {entities.map((entity, index) => (
+                    {entities.map((entity) => (
                         <Entity
                             key={entity.uuid}
                             name={entity.name}
