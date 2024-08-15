1a27e14689d7fb90183da1aff5d1836304bd0299
davidhorvat <david.horvat@gauss.dev>
2024-08-15 18:30:32 +0200

    fix: env load

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+
+const env = loadEnv(process.cwd(), '')
 
 export default defineConfig({
 
@@ -18,5 +20,8 @@ export default defineConfig({
   preview: {
     host: '0.0.0.0',
     port: 5713,
+  },
+  define: {
+    __APP_ENV__: JSON.stringify(env.APP_ENV),
   },
 })
