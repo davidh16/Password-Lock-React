0aa3db00df59c529fedf191d6b26824805bc6709
davidhorvat <david.horvat@gauss.dev>
2024-08-15 15:42:39 +0200

    chore: add logs to axios instance

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
     port: 5713,
     proxy: {
       '/api': {
-        target: import.meta.env.VITE_LOCAL_BASE_URL,
+        target: process.env.LOCAL_BASE_URL,
         changeOrigin: true,
         rewrite: path => path.replace(/^\/api/, ''),
       },
