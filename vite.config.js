5c6d467cc1954a6bb46f31b72c2e351887ab27bd
davidhorvat <david.horvat@gauss.dev>
2024-08-15 18:38:15 +0200

    fix: env load

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,31 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const env = loadEnv(process.cwd(), '')
 
-export default defineConfig({
 
-  plugins: [react()],
-  server: {
-    host: '0.0.0.0',
-    port: 5713,
-    proxy: {
-      '/api': {
-        target: "http://backend:8080",
-        changeOrigin: true,
-        rewrite: path => path.replace(/^\/api/, ''),
+export default defineConfig(({ mode }) =>{
+
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return{
+    plugins: [react()],
+    server: {
+      host: '0.0.0.0',
+      port: 5713,
+      proxy: {
+        '/api': {
+          target: "http://backend:8080",
+          changeOrigin: true,
+          rewrite: path => path.replace(/^\/api/, ''),
+        },
       },
     },
-  },
-  preview: {
-    host: '0.0.0.0',
-    port: 5713,
-  },
-  define: {
-    __APP_ENV__: JSON.stringify(env.APP_ENV),
-  },
+    preview: {
+      host: '0.0.0.0',
+      port: 5713,
+    },
+    define: {
+      __APP_ENV__: JSON.stringify(env.APP_ENV),
+    },
+  }
 })
