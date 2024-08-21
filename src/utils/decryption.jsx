7b99aeb9e7ed6ab4b79584b35bda12303604269e
davidhorvat <david.horvat@gauss.dev>
2024-08-21 20:35:29 +0200

    chore: add log

--- src/utils/decryption.jsx
+++ src/utils/decryption.jsx
@@ -17,7 +17,44 @@ export function decryptResponse(cipherText){
     });
 
     console.log(decrypted)
-    console.log(CryptoJS.enc.Utf8.stringify(decrypted))
+
+    try {
+        const base64String = CryptoJS.enc.Base64.stringify(decrypted);
+        console.log("Decrypted Base64 String:", base64String);
+    } catch (e) {
+        console.error("Error converting decrypted data to BASE64:", e.message);
+        // Handle the error, perhaps by logging the raw data or using a fallback encoding
+    }
+
+    try {
+        const hexString = CryptoJS.enc.Hex.stringify(decrypted);
+        console.log("Decrypted Hex String:", hexString);
+    } catch (e) {
+        console.error("Error converting decrypted data to hex:", e.message);
+        // Handle the error, perhaps by logging the raw data or using a fallback encoding
+    }
+
+    try {
+        const decryptedBuffer = Buffer.from(decrypted.toString(CryptoJS.enc.Hex), 'hex');
+        const utf8String = decryptedBuffer.toString('utf8');
+        console.log("Decrypted UTF-8 String using Buffer:", utf8String);
+    } catch (e) {
+        console.error("Error converting decrypted data to BUFFER", e.message);
+        // Handle the error, perhaps by logging the raw data or using a fallback encoding
+    }
+
+    try {
+        const utf8String = CryptoJS.enc.Utf8.stringify(decrypted);
+        console.log("Decrypted UTF-8 String:", utf8String);
+    } catch (e) {
+        console.error("Error converting decrypted data to UTF-8:", e.message);
+        // Handle the error, perhaps by logging the raw data or using a fallback encoding
+    }
+
+
+
+
+
 
     return CryptoJS.enc.Utf8.stringify(decrypted);
 }
